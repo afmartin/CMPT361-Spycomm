dd0426b6a937ee050a91b54ea5ac409f9220d9a9
Kevin Saforo <saforok@students.cs.macewan.ca>
2015-11-11 18:17:23 -0700

    Add read/write file functions to file.c and file.h
    Added the code to read a file into a uint8_t array, and then
    another function to turn that array into a file. Look at
    file.h for a detailed explanation.

--- file.c
+++ file.c
@@ -10,29 +10,88 @@ Description:
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdint.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-int getFileSize(int fd, char* filename) {
+int getFileSize(int fd) {
   struct stat buf;
   int size;
   
   fstat(fd, &buf);
   size = buf.st_size;
 
-  printf("Size of %s is %d\n", filename, size);
+  return size;
+}
+
+int getFileArray(FILE* file, int fileSize,  uint8_t *byteArray) {
+  size_t newLen = fread(byteArray, 1, fileSize, file);
+  if (newLen == 0) {
+    fprintf(stderr, "Error reading file\n");
+    return -1;
+  } else {
+    byteArray[newLen++] = '\0';
+  }
   return 0;
 }
 
+int writeToFile(char* filename, uint8_t *byteArray, int fileSize) {
+  int i, k;
+  uint8_t chr;
+  FILE *fp;
+
+  k = (fileSize / sizeof(uint8_t));
+
+  /* referenced from stackoverflow.com/questions/13002367/write-a-file
+     -byte-by-byte-in-c-using-fwrite */
+  fp = fopen(filename, "w+");
+  if (fp == NULL) {
+    fprintf(stderr, "Error opening file '%s'\n", filename);
+    return -1;
+  }
+
+  for (i = 0; i < k; i++) {
+    chr = byteArray[i];
+    if (chr == EOF) {
+      break;
+    }
+    char c = (char) chr;
+    fwrite(&c, 1, sizeof(c), fp);
+  }
+
+  close(fp);
+  return 0;
+}
+
+void printByteArray(uint8_t* byteArray, int fileSize) {
+  int i, k;
+  k = (fileSize / sizeof(uint8_t));  
+
+  for (i = 0; i < k; i++) 
+    fprintf(stdout, "%x ", byteArray[i]);
+}
+	       
+/*main function used to test above functions
 
 int main(void) {
-  FILE *fp;
-  int fd;
+  FILE *fp, *fp2;
+  int fd, size, i, k;
+  uint8_t *array;
   char filename[] = "server.c";
+  char filename2[] = "asdf.c";
+
   
   fp = fopen(filename, "r");
   fd = fileno(fp);
-  getFileSize(fd, filename);
+  size = getFileSize(fd);
+  k = (size / sizeof(uint8_t));
+  array = (uint8_t*) malloc((size+1)*sizeof(uint8_t));
+  getFileArray(fp, size, array);
+  close(fd);
+  printByteArray(array, size);
+  writeToFile(filename2, array, size);
+  free(array);
   return 0;
-}
+  } */
+
---
+++ file.h
@@ -0,0 +1,34 @@
+/*
+#############################################################
+CMPT 361 - Assignment 3
+Group 4: Nick, John, Alex, Kevin
+November 11th, 2015
+Filename: file.h
+Description:
+#############################################################
+*/
+
+#ifndef _FILE_H
+#define _FILE_H
+
+int getFileSize(int fd);
+/* The function takes a file descriptor as parameters and
+   returns the size of the file in bytes. */
+
+int getFileArray(FILE* file, int fileSize, uint8_t *byteArray);
+/* Takes a file pointer, the size of the file in bytes, and a
+   pointer to a uint8_t array as parameters. Puts the contents
+   of the file into the array. Returns 0 on success and -1
+   on failure. */
+
+int writeToFile(char* filename, uint8_t *byteArray, int fileSize);
+/* Takes string of the filename to be created, a pointer to a
+   uint8_t array containing bytes to be written, and an integer
+   representing the amount of bytes to be written. Will write
+   to the file and return 0 on success and -1 on failure. */
+
+void printByteArray(uint8_t* byteArray, int fileSize);
+/* Takes uint8_t array containing bytes to be printed, and the
+   total size of the array. */
+
+#endif /* end _FILE_H if */
