69979332a1b9dceae78eb5ac202acf4ca3a6b9d3
John Mulvany-Robbins <mulvanyrobbinsj@students.cs.macewan.ca>
2015-11-21 21:34:56 -0700

    whats life without color... got the UI working :)

---
+++ screen.h
@@ -0,0 +1,28 @@
+
+
+#define MAXBOXES 8
+#define HEIGHT 7
+#define WIDTH 36
+#define MAXBAR 34
+#define COLUMN1 0
+#define COLUMN2 WIDTH+2
+
+typedef struct _displayBox{
+  
+  int boxNo;
+  int row;
+  int column;
+  int progress;
+  int position;
+  float percentage;
+  int state;
+
+} Box; 
+
+
+Box * initBoxes();
+void drawBox(Box box);
+void connectedToDisplay(Box box, char * clientAddr, char * fileName);
+void progressBar(Box * box, long fileSize);
+void clearBox(Box box);
+void endBox(Box * box);
