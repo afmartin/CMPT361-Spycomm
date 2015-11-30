0495122ea926a594168eea3a94e50342de981acd
edwardsn8 <edwardsn8@mymacewan.ca>
2015-11-30 11:13:18 -0700

    Readding server.h for now

---
+++ server.h
@@ -0,0 +1,30 @@
+/*
+#############################################################
+CMPT 361 - Assignment 3
+Group 4: Nick, John, Alex, Kevin
+November 20th, 2015
+Filename: server.h
+Description:
+#############################################################
+*/
+
+#ifndef _SERVER_H
+#define _SERVER_H
+
+typedef struct _fileInfo fileInfo;
+
+//This function prints out the usage for the spycommd exec.
+void printUsage(char* name);
+
+//Grab a socket and return it. Exits on failure.
+int getSocket(char* port);
+
+int getPadOffset(int padID);
+
+int initFileTransfer(int cd, fileInfo *info, long long int * padSize, long long int * padOffset);
+
+void* worker(void * arg);
+
+void inputHandler(int s);
+
+#endif /* end _SERVER_H if */
